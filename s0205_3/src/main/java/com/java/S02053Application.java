c76ca83a5db1ce4f25cb4eedb08fb6a8de7de1bb
lim <mylim52@naver.com>
2025-02-05 18:31:00 +0900

    Spring01

---
+++ s0205_3/src/main/java/com/java/S02053Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S02053Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S02053Application.class, args);
+	}
+
+}
